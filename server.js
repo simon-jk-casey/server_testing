ab02713cf8271edc0d6b8efa9e1ac0a7c0b5adc6
Simon Casey <simon.jk.casey@gmail.com>
2016-10-24 18:43:49 +1300

    setup server config

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const express = require('express')
 const hbs = require('handlebars')
-
 const routes = require('./routes')
+
+const server = express()
+
+module.exports = server
+
+//CONFIGURATION
+//middleware
+server.engine('hbs', hbs ({
+  extname: 'hbs',
+  defaultLayout: 'main'
+}))
+
+server.set('view engine', 'hbs')
+server.set('views', path.join(__dirname, 'views'))
+
+//google handlebars and css to style (PUBLIC folder with css inside)
